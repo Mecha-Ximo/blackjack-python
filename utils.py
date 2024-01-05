c39ce37c9e28c35b1d71092b7a90fc1700ac18b5
Ximo <ximo.dev.94@gmail.com>
2024-01-05 16:20:14 +0100

    feat: add utils

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+PLAYER_OPTIONS = {
+    'SEE_HAND': 1,
+    'SEE_HAND_VALUE': 2,
+    'TAKE_CARD': 3,
+    'FINISH': 4
+}
+
 def highlight_log(fn):
     def wrapper(*args,**kwargs):
         print(f"\n{'#' * 20}")
@@ -6,3 +13,19 @@ def highlight_log(fn):
         return res
     
     return wrapper
+
+def input_with_validation(prompt: str, options: list[str] | list[int]) -> str | int:
+    while True:
+        user_input = input(prompt)
+
+        if type(options[0]) == str:
+            if user_input in options:
+                return user_input
+        if type(options[0]) == int:
+            if user_input.isdigit():
+                int_input = int(user_input)
+                if int_input in options:
+                    return int_input
+        
+        print("Invalid input")
+        
