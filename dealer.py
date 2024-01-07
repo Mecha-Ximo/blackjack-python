b4d851f5090b34ceef636f8c6fdb9cd85e295d1c
Ximo <ximo.dev.94@gmail.com>
2024-01-07 10:45:28 +0100

    feat: implement dealer play algorithm

--- dealer.py
+++ dealer.py
@@ -20,6 +20,54 @@ class Dealer(Player):
             cards.append(self.deck.deal_card())
         return cards
 
+    def play_hand(self, player_result: int) -> bool | int:
+        print("Dealer hand")
+        print(self.hand)
+
+        if player_result > 21:
+            return True
+        
+        if player_result == 21:
+            return self.go_for_blackjack()
+        
+        if player_result < 21:
+            return self.go_for_value(17)
+    
+    def go_for_value(self, value: int):
+        possibilities = self.hand.get_valid_values()
+
+        values_in_range = list(filter(lambda v: v in range(value, 22), possibilities))
+        if len(values_in_range):
+            return max(values_in_range)
+
+        
+        if not len(possibilities):
+            return False
+        
+        new_card = self.deal_player_cards(1)
+        print("-> Dealer takes card")
+        print(new_card)
+        self.take_cards(new_card)
+
+        return self.go_for_value(value)
+    
+    def go_for_blackjack(self, value = 21):
+        possibilities = self.hand.get_valid_values()
+
+        if value in possibilities:
+            return True
+        
+        if not len(possibilities):
+            return False
+        
+        new_card = self.deal_player_cards(1)
+        print("-> Dealer takes card")
+        print(new_card)
+        self.take_cards(new_card)
+
+        return self.go_for_blackjack(value)
+
+
     @highlight_log
     def show_first_card(self):
         print(f"Dealer first card {self.hand.cards[0]}")
